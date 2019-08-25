5e9eca9da19c76560f0b30284ecd1d9895380977
MCApollo <34170230+MCApollo@users.noreply.github.com>
2019-08-24 19:06:30 -0700

    data: Added pcre

---
+++ data/pcre/make.sh
@@ -0,0 +1,4 @@
+pkg:setup
+pkg:configure --enable-utf8 --enable-pcre8 --enable-pcre16 --enable-pcre32 --enable-unicode-properties --enable-pcregrep-libz --enable-pcregrep-libbz2 --enable-jit
+make
+pkg:install 
