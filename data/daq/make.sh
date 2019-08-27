3eb971e374da416657c3d4d658561d791bd81ec5
MCApollo <34170230+MCApollo@users.noreply.github.com>
2019-08-27 09:30:49 -0700

    data/daq: use malloc

--- data/daq/make.sh
+++ data/daq/make.sh
@@ -1,4 +1,4 @@
 pkg:setup
-pkg:configure --disable-dependency-tracking --disable-silent-rules daq_cv_libpcap_version_1x=yes
+pkg:configure --disable-dependency-tracking --disable-silent-rules daq_cv_libpcap_version_1x=yes ac_cv_func_malloc_0_nonnull=yes ac_cv_func_realloc_0_nonnull=yes
 make	# on purpose
 pkg:install 
