209d502a16a951096e50597aadc361be12fc70a3
MCApollo <34170230+MCApollo@users.noreply.github.com>
2019-08-30 22:38:45 -0700

    data/libnet: Actually use bpf sources

--- data/libnet/make.sh
+++ data/libnet/make.sh
@@ -1,5 +1,6 @@
+# From elucubratus.
 pkg:setup
 autoreconf -fvi
-pkg:configure --disable-dependency-tracking
+pkg:configure --with-link-layer=bpf --disable-dependency-tracking ac_cv_libnet_endianess=lil ac_libnet_have_packet_socket=no ac_cv_libnet_linux_procfs=no ac_cv_lbl_unaligned_fail=no
 sed -i 's|#include <net/bpf.h>||' src/libnet_link_bpf.c 
 pkg:install 
