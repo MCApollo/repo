230b405f6b3c70de774e950136e7072172918b62
MCApollo <34170230+MCApollo@users.noreply.github.com>
2019-10-15 17:57:37 -0700

    data/libusb: (bad) fixes to go with lastest version of macos

--- data/libusb/make.sh
+++ data/libusb/make.sh
@@ -1,3 +1,7 @@
 pkg:setup
 pkg:configure
 pkg:install 
+
+# Create symlink for libusb/libusb.h
+shopt -s extglob
+ln -rs "${PKG_DEST}/${PKG_TAPF}"/include/libusb-* "${PKG_DEST}/${PKG_TAPF}"/include/libusb
