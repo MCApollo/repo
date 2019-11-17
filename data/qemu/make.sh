c4adadc17e6d7644f9dd7134195b175e16793468
MCApollo <34170230+MCApollo@users.noreply.github.com>
2019-11-16 23:42:01 -0800

    data/qemu: mark as debug for now.

--- data/qemu/make.sh
+++ data/qemu/make.sh
@@ -4,5 +4,9 @@ mkdir bin
 printf '#!/bin/bash\n%s $@' "${PKG_BASE}/util/pkg-config.sh" > bin/${PKG_TARG}-pkg-config
 chmod +x bin/${PKG_TARG}-pkg-config
 
-PATH="${PWD}/bin:${PATH}" ./configure --cpu=aarch64 --cross-prefix=${PKG_TARG}- --enable-curses --extra-cflags=-DNCURSES_WIDECHAR=1 --disable-cocoa --disable-sdl --disable-gtk --prefix=${PKG_TAPF} --enable-debug-info --enable-debug --target-list=i386-softmmu --enable-tcg-interpreter --enable-debug-info --enable-debug --disable-capstone --enable-pie  --enable-libssh --enable-vde 
+PATH="${PWD}/bin:${PATH}" ./configure --cpu=aarch64 --cross-prefix=${PKG_TARG}- --enable-curses --extra-cflags="-DNCURSES_WIDECHAR=1"  --disable-cocoa --disable-sdl --disable-gtk --prefix=${PKG_TAPF} --enable-pie --enable-libssh --enable-vde --disable-kvm --enable-modules --disable-coroutine-pool --disable-strip  --enable-tcg-interpreter --enable-debug-info --enable-debug
+# FIXME: gthreads on qemu.
+sed -i "s/CONFIG_COROUTINE_BACKEND=sigaltstack/CONFIG_COROUTINE_BACKEND=gthread/" config-host.mak
 pkg:install V=1
+
+# ldid -S${PKG_DATA}/qemu.xml ${PKG_DEST}/${PKG_TAPF}/bin/*
