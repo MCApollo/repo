caa8e17a81d01703cdc26108a693d560722e0a9e
MCApollo <34170230+MCApollo@users.noreply.github.com>
2019-11-16 21:08:30 -0800

    data/x264: append --enable-shared for -dev. files, change install_name.

--- data/x264/make.sh
+++ data/x264/make.sh
@@ -3,5 +3,5 @@ cp ${PKG_DATA}/gas-preprocessor.pl .
 AS="./gas-preprocessor.pl -arch $(echo ${PKG_TARG} | sed -e 's/-.*//') -- ${PKG_TARG}-gcc" \
 CC="${PKG_TARG}-gcc" \
 CFLAGS="-fno-stack-check" \
-pkg:configure --disable-lsmash --disable-swscale --disable-ffms --enable-strip --cross-prefix="${PKG_TARG}-"
+pkg:configure --disable-lsmash --disable-swscale --disable-ffms --enable-strip --cross-prefix="${PKG_TARG}-" --enable-shared
 make install DESTDIR=${PKG_DEST}
