e655975a5e952688ffd87c64f4b0c03e1921b45a
MCApollo <34170230+MCApollo@users.noreply.github.com>
2020-01-15 10:55:09 -0800

    data/pixman: symlink include/pixman

--- data/pixman/make.sh
+++ data/pixman/make.sh
@@ -1,3 +1,5 @@
 pkg:setup
 pkg:configure --disable-dependency-tracking --disable-gtk --disable-silent-rules 
 pkg:install 
+
+ln -rs ${PKG_DEST}/${PKG_TAPF}/include/pixman-*/* ${PKG_DEST}/${PKG_TAPF}/include/
