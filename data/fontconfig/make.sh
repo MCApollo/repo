3de41476409b6192099b967732ff18b2455c015a
MCApollo <34170230+MCApollo@users.noreply.github.com>
2020-01-18 17:19:43 -0800

    data/fontconfig: fixed crash

--- data/fontconfig/make.sh
+++ data/fontconfig/make.sh
@@ -1,4 +1,5 @@
 pkg:setup
-pkg:configure --sysconfdir=${PKG_TAPF}/etc
+pkg:configure --sysconfdir=${PKG_TAPF}/etc --with-default-fonts=${PKG_TAPF}/share/fonts
+echo "#define FC_NO_MT 1" >> config.h
 make
 pkg:install RUN_FC_CACHE_TEST=false
