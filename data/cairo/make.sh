790dfab8852f53c772a2f0ac01a53a2b89aa1791
MCApollo <34170230+MCApollo@users.noreply.github.com>
2020-01-26 16:59:11 -0800

    data/cairo: FIXME: X11 built-in for GTK+3

--- data/cairo/make.sh
+++ data/cairo/make.sh
@@ -1,4 +1,17 @@
 pkg:setup
-pkg:configure --disable-dependency-tracking  --enable-gobject=yes --enable-svg=yes --enable-tee=yes --enable-xcb=no --enable-xlib=no --enable-xlib-xrender=no	\
-	CFLAGS="-I$(PKG_DEST_ pixman)/${PKG_TAPF}/include/pixman-1 -I$(PKG_DEST_ glib)/${PKG_TAPF}/include/glib-2.0 -I$(PKG_DEST_ glib)/${PKG_TAPF}/lib/glib-2.0/include"
+
+# FIXME: cairo-X11 
+#if [[ -d "$(PKG_DEST_ Xorg)/${PKG_TAPF}" ]]; then 
+#CFLAGS="-I$(PKG_DEST_ Xorg)/${PKG_TAPF}/include"
+#LDFLAGS="-L$(PKG_DEST_ Xorg)/${PKG_TAPF}/lib"
+#X_FLAGS='--enable-xcb=yes --enable-xlib=yes --enable-xlib-xrender=yes'
+#export PKG_CONFIG_PATH="$(PKG_DEST_ Xorg)/${PKG_TAPF}/lib/pkgconfig"
+#else
+#X_FLAGS='--enable-xcb=no --enable-xlib=no --enable-xlib-xrender=no'
+#echo -e "\n\n#####\nWARNING: Cairo-X11 is not being built! './make.sh Xorg'\n#####\n\n"; sleep 5
+#fi
+
+PKG_CONFIG=${PKG_DATA}/pkg-config.sh \
+pkg:configure --disable-dependency-tracking  --enable-gobject=yes --enable-svg=yes --enable-tee=yes ${X_FLAGS} \
+	CFLAGS="-I$(PKG_DEST_ pixman)/${PKG_TAPF}/include/pixman-1 -I$(PKG_DEST_ glib)/${PKG_TAPF}/include/glib-2.0 -I$(PKG_DEST_ glib)/${PKG_TAPF}/lib/glib-2.0/include ${CFLAGS}" LDFLAGS="${LDFLAGS}" 
 pkg:install 
---
+++ data/cairo/pkg-config.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+declare -a args
+declare -a pkgs
+unset dbpf
+export PKG_TAPF=$(cat "${PKG_BASE}/arch/${PKG_ARCH}/${PKG_CFTARG}/prefix")
+
+while [[ $# -ne 0 ]]; do case "$1" in
+    (--version)
+	echo "0.29.1"
+	exit 0
+    ;;
+    (--atleast-pkgconfig-version)
+        exec pkg-config "$1" "$2"
+    ;;
+
+    (--cflags|--libs|--libs-only-l|--libs-only-L|--cflags-only-I|--variable=*)
+        dbpf=
+        args[${#args[@]}]=$1
+    ;;
+
+    (--errors-to-stdout|--exists|--modversion|--print-errors|--short-errors|--uninstalled|--debug)
+        args[${#args[@]}]=$1
+    ;;
+
+    (--atleast-version|--exact-version|--max-version)
+        args[${#args[@]}]=$1
+        args[${#args[@]}]=$2
+        shift
+    ;;
+
+		(--atleast-version=*)
+        args[${#args[@]}]=--atleast-version
+				args[${#args[@]}]=$(echo $1 | cut -d = -f 2)
+    ;;
+
+    (--*)
+        echo "unknown pkg-config option $1" 1>&2
+        exit 1
+    ;;
+
+    (*)
+        pkgs[${#pkgs[@]}]=$1
+    ;;
+esac; shift; done
+
+if [[ ${dbpf+@} ]]; then
+    source "${PKG_BASE}/lib/folders.sh"
+fi
+
+function join_by { local IFS="$1"; shift; echo "$*"; }
+declare -a PKG_SEARCH_PATH
+for dep in $(find -L "${PKG_DATA}"/_metadata -name '*.dep'; echo "_metadata/Xorg.dep"); do
+    DEP_NAME=$(basename "${dep}" .dep)
+    DEP_DEST=$(PKG_DEST_ "${DEP_NAME}")
+#    echo @@@ "$DEP_NAME" "$DEP_DEST" 2>&1
+    PKG_SEARCH_PATH+=( $(find "${DEP_DEST}" -type d -name pkgconfig | head -n 1) )
+done
+export PKG_CONFIG_PATH=$(join_by : ${PKG_SEARCH_PATH[*]})
+
+outs=
+for pkg in "${pkgs[@]}"; do
+    declare -a pkgspec
+    pkgspec=( ${pkg} )
+    args_=("${args[@]}")
+    destdir=
+
+    # Locate the file in the search path and get the destdir.
+    destdir=$(find ${PKG_SEARCH_PATH[@]} -name ${pkg}.pc); destdir=${destdir%%${PKG_TAPF}*}
+
+    out=$(pkg-config "${args_[@]}" "${pkg}") || exit $?
+    # Replace out prefix with $destdir/prefix
+    out=${out//${PKG_TAPF}/${destdir}/${PKG_TAPF}}
+    outs+=\ ${out}
+done
+
+echo "${out#\ }"
