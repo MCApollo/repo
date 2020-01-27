841806e625db9cdb437f58913f959a42472c0c8f
MCApollo <34170230+MCApollo@users.noreply.github.com>
2020-01-27 14:42:20 -0800

    data: xfconf

---
+++ data/xfconf/make.sh
@@ -0,0 +1,22 @@
+if ! command -v intltool-update &>/dev/null; then
+echo 'brew install intltool'
+exit 127
+fi
+
+pkg:setup
+
+if ! perl -e 'require XML::Parser'; then
+# XML not found- homebrew issue intltool
+mkdir host; pushd host
+url='https://cpan.metacpan.org/authors/id/T/TO/TODDR/XML-Parser-2.44.tar.gz'
+wget ${url}
+tar xf ${url##*/}; rm ${url##*/}
+cd *
+perl Makefile.PL INSTALL_BASE=${PWD}/../
+make install
+popd
+fi
+
+PERL5LIB="${PWD}/host/lib/perl5/" \
+pkg:configure --enable-visibility=no #  --enable-maintainer-mode FIXME: HAVE_GNUC_VISIBILITY - iPhone.diff doesn't fix alias issues
+pkg:install
